4302f3a1b0a280fe3f95811e923201e0fce80ba4
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-06-12 16:16:18 -0700

    chore: :bulb: Removed eslint disable comments in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,13 +8,11 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
   jsdoc.configs["flat/recommended-typescript"],
   {
     ignores: [".prettierrc.js"],
   },
   {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     plugins: { jsdoc },
     languageOptions: {
       parserOptions: {
