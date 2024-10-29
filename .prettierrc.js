9483f5ed9d1b66c8c58a84f2c44aad5630e1cab5
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-10-29 10:36:39 -0700

    build: :heavy_minus_sign: remove eslint & prettier

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
-
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: ["@trivago/prettier-plugin-sort-imports"],
-  endOfLine: "auto",
-};
-
-export default config;
