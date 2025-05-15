eafab9135ea316dadc1be9a659412d49af02fcfc
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2025-05-15 15:45:23 -0700

    style: :rotating_light: `biome format --write`

--- tools/get-scales.ts
+++ tools/get-scales.ts
@@ -2,11 +2,11 @@ import { scrapeScales } from "./index.js";
 
 console.log(":root {");
 for await (const {
-  base,
-  incrementName,
-  remSize,
-  pixelSize,
+	base,
+	incrementName,
+	remSize,
+	pixelSize,
 } of scrapeScales()) {
-  console.log(`\t--${incrementName}: ${remSize}; /* ${pixelSize} ${base} */`);
+	console.log(`\t--${incrementName}: ${remSize}; /* ${pixelSize} ${base} */`);
 }
 console.log("}");
